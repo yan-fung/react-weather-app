3b2d8a42f38b3bd6c77d2c6328d859c026230092
Yan Fung <fungmingyan@hotmail.com>
2023-01-20 10:03:06 +0000

    Updated the App component by passing data from location props into LocationDetails component. Add PropTypes validation for the location prop.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import "../styles/App.css";
+import React from "react";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+function App({ location }) {
+  const { city, country } = location;
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
 }
 
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 export default App;
