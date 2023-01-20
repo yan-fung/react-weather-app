7074d4aa74eda063783403868425a9e8d0d20cac
Yan Fung <fungmingyan@hotmail.com>
2023-01-20 12:00:31 +0000

    Updated the test by adding location prop and changed the text being passed in in the getByText

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "../components/App";
+import forecast from "../data/forecast.json";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Weather App/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+  test('renders App component correctly', () => {
+    render(<App location={forecast.location} />);
+    const h1Element = screen.getByText(/Manchester, UK/i);
+    expect(h1Element).toBeInTheDocument();
+  });
+})
+
+
