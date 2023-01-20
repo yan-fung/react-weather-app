6c175f3d496c5b71678f05d6bc180af9f7fa0c7f
Yan Fung <fungmingyan@hotmail.com>
2023-01-20 13:55:12 +0000

    Added a snapshot testing for props - date, description, icon and temperature with test passing.

--- src/__tests__/components/ForecastSummary.test.js
+++ src/__tests__/components/ForecastSummary.test.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
+
 
 describe("ForecastSummary", () => {
-  it("renders", () => {});
-})
+  const validProps = {
+    date: 11100011,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+        <ForecastSummary
+          date={validProps.date}
+          description={validProps.description} 
+          icon={validProps.icon}
+          temperature={validProps.temperature} 
+          />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
