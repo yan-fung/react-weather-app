ce3f82c088e7a930c63fee00689f1342cd17a5cf
Yan Fung <fungmingyan@hotmail.com>
2023-01-21 19:01:17 +0000

    Imported the stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 
+import "../styles/App.css"
+
 function App({ location, forecasts }) {
   const { city, country } = location;
   return (
-    <div className="forecast">
+    <div className="weather-app">
       <LocationDetails city={city} country={country} />
       <ForecastSummaries forecasts={forecasts} />
     </div>
--- src/components/ForecastSummaries.js
+++ src/components/ForecastSummaries.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import ForecastSummary from "./ForecastSummary";
 
+import "../styles/ForecastSummaries.css";
+
 function ForecastSummaries({ forecasts }) {
   return (
     <div className="forecast-summaries">
