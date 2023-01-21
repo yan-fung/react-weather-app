470c6d719c28d8392ecd576c8c3ef827df25a42b
Yan Fung <fungmingyan@hotmail.com>
2023-01-21 09:53:48 +0000

    imported ForecastSummaries and added ForecastSummaries with forecasts attribute.

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,34 @@ import "../styles/App.css";
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
+function App({ location, forecasts }) {
   const { city, country } = location;
   return (
-    <div className="App">
+    <div className="forecast">
       <LocationDetails city={city} country={country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
 };
+
 export default App;
