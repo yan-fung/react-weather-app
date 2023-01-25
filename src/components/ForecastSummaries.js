2b30a4cb7525d2ac00c6a27fc741e71db11c9e4e
Yan Fung <fungmingyan@hotmail.com>
2023-01-25 21:22:55 +0000

    Updated the prop for icon by adding toString() to convert it as a string for passing down. Updated the proptypes validation for icon from string to number to do our request for forecast data

--- src/components/ForecastSummaries.js
+++ src/components/ForecastSummaries.js
@@ -12,7 +12,7 @@ function ForecastSummaries({ forecasts, onForecastSelect }) {
           key={forecast.date}
           date={forecast.date}
           description={forecast.description}
-          icon={forecast.icon}
+          icon={forecast.icon.toString()}
           onSelect={onForecastSelect}
           temperature={forecast.temperature}
         />
@@ -26,7 +26,7 @@ ForecastSummaries.propTypes = {
     PropTypes.shape({
       date: PropTypes.number,
       description: PropTypes.string,
-      icon: PropTypes.string,
+      icon: PropTypes.number,
       temperature: PropTypes.shape({
         max: PropTypes.number,
         min: PropTypes.number,
