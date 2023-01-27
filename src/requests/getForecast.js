447d4bef00a6f857a937950c57ec1c634d3ee076
Yan Fung <fungmingyan@hotmail.com>
2023-01-27 09:38:53 +0000

    Modify endpoint in getForecast() so it can return the forecast for a particular city

--- src/requests/getForecast.js
+++ src/requests/getForecast.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 
-const getForecast = (setSelectedDate, setForecasts, setLocation) => {
-  const endpoint = "https://cmd-shift-weather-app.onrender.com/forecast";
+const getForecast = (
+  setSelectedDate,
+  setForecasts,
+  setLocation,
+  searchText
+) => {
+  let endpoint = "https://cmd-shift-weather-app.onrender.com/forecast";
 
-  axios.get(endpoint).then((response) => {
+  if (searchText) {
+    endpoint += `?city=${searchText}`;
+  }
+
+  return axios.get(endpoint).then((response) => {
     setSelectedDate(response.data.forecasts[0].date);
     setForecasts(response.data.forecasts);
     setLocation(response.data.location);
