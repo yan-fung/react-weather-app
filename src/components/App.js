c59bb1170776d4e68cbab548bebc8468db9f06f8
Yan Fung <fungmingyan@hotmail.com>
2023-01-27 11:18:44 +0000

    Add a new state variable searchText and set its initial state to an empty string. Passed searchText and setSearchText to SearchForm component as props. In handleInputChange() use setSearchText() to set the value of searchText. Created handleCitySearch() to make another getForecast() request

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,38 @@ import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
+import SearchForm from "./SearchForm";
 import "../styles/App.css";
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
-
+  const [searchText, setSearchText] = useState("");
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
-
-  const handleForecastSelect = (date) => {
-    setSelectedDate(date);
-  };
 
   useEffect(() => {
     getForecast(setSelectedDate, setForecasts, setLocation);
   }, []);
 
+  const handleForecastSelect = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleCitySearch = () => {
+    getForecast(setSelectedDate, setForecasts, setLocation, searchText);
+  };
+
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
