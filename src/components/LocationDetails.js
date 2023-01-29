810ba40b1d3881902e33865255e124051ed87e4f
Yan Fung <fungmingyan@hotmail.com>
2023-01-29 11:20:36 +0000

    Passed errorMessage as prop and set the defaultProps as empty string for errMessage. Added errorMessage's proptypes

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -1,14 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../styles/LocationDetails.css";
 
 function LocationDetails(props) {
-  const { city, country } = props;
-  return <h1>{`${city}, ${country}`}</h1>;
+  const { city, country, errorMessage } = props;
+  return errorMessage ? (
+    <h1>{errorMessage}</h1>
+  ) : (
+    <div className="location-details">{`${city}, ${country}`}</div>
+  );
 }
+
+LocationDetails.defaultProps = {
+  errorMessage: "",
+};
 
 LocationDetails.propTypes = {
   city: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
 };
 
 export default LocationDetails;
